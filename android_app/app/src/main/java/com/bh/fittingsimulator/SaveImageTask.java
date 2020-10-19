82c400a9e8c0b737f53e8b96e2a3eaf5c7e792cf
splo2t <yu990410@naver.com>
2020-10-19 13:12:41 +0900

    add server

---
+++ android_app/app/src/main/java/com/bh/fittingsimulator/SaveImageTask.java
@@ -0,0 +1,125 @@
+package com.bh.fittingsimulator;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class SaveImageTask extends AsyncTask<Bitmap, Void, Void> {
+    CalibrationActivity calibrationActivity;
+
+    public SaveImageTask(CalibrationActivity calibrationActivity) throws IOException {
+        this.calibrationActivity = calibrationActivity;
+    }
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        Toast.makeText(calibrationActivity, "사진을 저장하였습니다.", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected Void doInBackground(Bitmap... data) {
+        Bitmap bitmap = null;
+        try {
+            bitmap = data[0];
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        insertImage(calibrationActivity.getContentResolver(), bitmap, ""+System.currentTimeMillis(), "");
+        return null;
+    }
+
+
+    //출처 - https://codeday.me/ko/qa/20190310/39556.html
+    /**
+     * A copy of the Android internals  insertImage method, this method populates the
+     * meta data with DATE_ADDED and DATE_TAKEN. This fixes a common problem where media
+     * that is inserted manually gets saved at the end of the gallery (because date is not populated).
+     * @see MediaStore.Images.Media#insertImage(ContentResolver, Bitmap, String, String)
+     */
+
+
+    public static final String insertImage(ContentResolver cr,
+                                           Bitmap source,
+                                           String title,
+                                           String description) {
+
+        Document doc;
+        String sUrl = "http://127.0.0.1:5000/test";
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, title);
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, title);
+        values.put(MediaStore.Images.Media.DESCRIPTION, description);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        // Add the date meta data to ensure the image is added at the front of the gallery
+        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+
+        Uri url = null;
+        String stringUrl = null;    /* value to be returned */
+        Elements element;
+        Connection.Response res;
+        try {
+            url = cr.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+            if (source != null) {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                File file = new File(Environment.getExternalStorageDirectory() + "/myimage.png");
+                FileOutputStream fOut = new FileOutputStream(file);
+                OutputStream imageOut = cr.openOutputStream(url);
+                try {
+                    source.compress(Bitmap.CompressFormat.JPEG, 50, fOut);
+                    fOut.close();
+                    res = Jsoup.connect(sUrl).data("file",file.getName(), new FileInputStream(file)).method(Connection.Method.GET).execute();
+                    element = res.parse().select("h1");
+                    String returnString = element.text();
+                    Log.d("abc", returnString);
+                } finally {
+                    imageOut.close();
+                }
+
+            } else {
+                cr.delete(url, null, null);
+                url = null;
+            }
+        } catch (Exception e) {
+            if (url != null) {
+                cr.delete(url, null, null);
+                url = null;
+            }
+        }
+
+        if (url != null) {
+            stringUrl = url.toString();
+        }
+
+        return stringUrl;
+    }
+
+
+
+
+
+
+
+}
--- android_app/app/src/main/java/com/bh/fittingsimulator/glrender/MyGLRenderer.java
+++ android_app/app/src/main/java/com/bh/fittingsimulator/glrender/MyGLRenderer.java
@@ -20,7 +20,7 @@ class MyGLRenderer implements GLSurfaceView.Renderer {
         //color buffer를 클리어할 때 사용할 색을 지정합니다.
         //red, green, blue, alpha 순으로 0~1사이의 값을 지정합니다.
         //여기에서는 검은색으로 지정하고 있습니다.
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     }
 
     //GLSurfaceView가 다시 그려질때 마다 호출되는 메소드입니다.
