cfe13e60eb5ceee2ac4f03f6f9006af8292d89ea
KBOHYUN <kbh5640@naver.com>
2020-10-31 17:16:51 +0900

    add android_app

--- android_app/app/src/main/java/com/bh/fittingsimulator/MainActivity.java
+++ android_app/app/src/main/java/com/bh/fittingsimulator/MainActivity.java
@@ -2,14 +2,23 @@ package com.bh.fittingsimulator;
 
 import android.content.Intent;
 import android.opengl.GLSurfaceView;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import com.bh.fittingsimulator.glrender.MyGLSurfaceView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,22 +31,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         testLayout = findViewById(R.id.mainlayout);
+
+        //툴바 설정
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+
+
         FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
-        params.setMargins(0,50,0,300);
+        params.setMargins(0,150,0,230);
         mGLView = new MyGLSurfaceView(this);
         mGLView.setLayoutParams(params);
         //mGLView.setForegroundGravity(Gravity.CENTER_HORIZONTAL);
         testLayout.addView(mGLView);
 
 
-        //testcode
-        Button btn = findViewById(R.id.testbutton);
+        //확인버튼
+        ImageButton btn = findViewById(R.id.testbutton);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,CalibrationActivity.class);
+                Intent intent=new Intent(MainActivity.this,ModeleingSuccessActivity.class);
                 startActivity(intent);
             }
         });
     }
+
+    //툴바 설정 메뉴 만들기
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_option, menu);
+        return true;
+    }
+
+    @Override //툴바 메뉴
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{//뒤로가기
+                finish();
+                return true;
+            }
+            case R.id.menu_calib:{
+                Toast.makeText(this, "캘리브레이션 이동", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.menu_modeling:{
+                Toast.makeText(this, "모델링 이동", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
